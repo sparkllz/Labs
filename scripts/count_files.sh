75c8af9b361c5baca6e70779cd9eae6420d52a72
sparkllz <sparklzzz@ukr.net>
2024-12-18 21:50:28 +0200

    first commit

---
+++ scripts/count_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d "/etc" ]; then
+  echo "/etc не існує"
+  exit 1
+fi
+
+count=$(find /etc -type f | wc -l)
+
+echo "Кількість файлів у каталозі /etc (без директорій та посилань): $count"
+
+# Polosmak Danil PI-221
